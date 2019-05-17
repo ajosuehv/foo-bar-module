48959c753ee0ac8a0bd52557fd40c612eec47d55
Aldo Josue Huerta-Verde <ahuerta@zahoree.com>
2019-05-17 08:08:22 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # foo-bar-module
 Solution to coding test
+
+In order to run in terminal, type:
+
+`$ ruby foo_bar.rb 100 5 7 `
+
+
--- foo_bar.rb
+++ foo_bar.rb
@@ -1,23 +1,24 @@
+#! /usr/bin/ruby
+class CodingTest
+  def generate_mod_array mod, array, code
+    mod = array.map{ |x| x%mod }
+    mod.join(',').gsub(',0',','+code).gsub(/[i0123456789]/,' ').split(',')
+  end
 
-def generate_mod_array mod, array, code
-    mod = array.map{ |x| x%modulus }
-    mod.join(',').gsub('0',code).gsub(/[1234]/,'').split(',')
-end
-
-def remove_multiples array, m1, m2
+  def remove_multiples array, m1, m2
     array.map{|x| if x%m1 != 0 and x%m2 !=0 then x end }
+  end
+
+  def main max, multiple1, multiple2
+    list = (1..max).map { |x| x}
+    numbers = remove_multiples list, multiple1, multiple2
+    mod_m1 = generate_mod_array multiple1, list, 'Foo'
+    mod_m2 = generate_mod_array multiple2, list, 'Bar'
+    foo_bar = mod_m1.zip(mod_m2).map{|x| x.join('')}
+    final = foo_bar.zip(numbers).map{|x| x.join('').gsub(' ','')}
+    puts final
+  end
 end
 
-list = (1..100).map { |x| x}
-
-numbers = remove_multiples list, 3, 5
-
-mod3 = generate_mod_array 3, list, 'Foo' 
-mod5 = generate_mod_array 5, list, 'Bar'
-
-foo_bar = mod3.zip(mod5).map{|x| x.join('')}
-
-final = foo_bar.zip(numbers).map{|x| x.join('')}
-
-puts final
-
+coding_test = CodingTest.new
+coding_test.main ARGV[0].to_i, ARGV[1].to_i, ARGV[2].to_i
